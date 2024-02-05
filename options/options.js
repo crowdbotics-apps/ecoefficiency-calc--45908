8d8b43df8d29f1c3b3a50d9f166b395f3aa744cf
Crowdbotics <team@crowdbotics.com>
2024-02-05 15:48:55 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ecoefficiency-calc--45908",
+  url: "https://ecoefficiency-calc--45908.botics.co",
+  api: "https://ecoefficiency-calc--45908.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
